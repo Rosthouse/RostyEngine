ef9f594cd99d21484f61f3d7acf17045a351e89b
rosthouse@gmail.com <rosthouse@gmail.com>
2015-05-07 23:04:51 +0200

    * Editing Level 2 to something playable

---
+++ assets/maps/Level2/Level2.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Level2" tilewidth="128" tileheight="136">
+ <tile id="0">
+  <image width="128" height="136" source="../../Tiles/Self/zelda-old-man.png"/>
+ </tile>
+</tileset>
--- assets/maps/tilesets/LevelObjects.tsx
+++ assets/maps/tilesets/LevelObjects.tsx
@@ -1,15 +1,54 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="LevelObjects" tilewidth="32" tileheight="32">
- <tile id="0">
+ <terraintypes>
+  <terrain name="Lava" tile="0"/>
+ </terraintypes>
+ <tile id="0" terrain="0,0,0,0">
   <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava0.png"/>
  </tile>
- <tile id="1">
+ <tile id="1" terrain="0,0,0,0">
   <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava1.png"/>
  </tile>
- <tile id="2">
+ <tile id="2" terrain="0,0,0,0">
   <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava2.png"/>
  </tile>
- <tile id="3">
+ <tile id="3" terrain="0,0,0,0">
   <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava3.png"/>
+ </tile>
+ <tile id="4" terrain=",0,,">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_corner_NE.png"/>
+ </tile>
+ <tile id="5" terrain="0,,0,0">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_corner_NE_inv.png"/>
+ </tile>
+ <tile id="6" terrain="0,,,">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_corner_NW.png"/>
+ </tile>
+ <tile id="7" terrain=",0,0,0">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_corner_NW_inv.png"/>
+ </tile>
+ <tile id="8" terrain=",,,0">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_corner_SE.png"/>
+ </tile>
+ <tile id="9" terrain="0,0,0,">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_corner_SE_inv.png"/>
+ </tile>
+ <tile id="10" terrain=",,0,">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_corner_SW.png"/>
+ </tile>
+ <tile id="11" terrain="0,0,,0">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_corner_SW_inv.png"/>
+ </tile>
+ <tile id="12" terrain=",0,,0">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_E.png"/>
+ </tile>
+ <tile id="13" terrain="0,0,,">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_N.png"/>
+ </tile>
+ <tile id="14" terrain=",,0,0">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_S.png"/>
+ </tile>
+ <tile id="15" terrain="0,,0,">
+  <image width="32" height="32" source="../../Tiles/dc-dngn/floor/lava_W.png"/>
  </tile>
 </tileset>
